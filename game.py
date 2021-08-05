996218c03637b034bab1eb943fe3a0120ef490e9
cristian <cristian@debian>
2021-08-05 12:29:08 -0300

    fixed movimentation issue, by rounding the x and y coordinates when moving in certain angle

--- game.py
+++ game.py
@@ -9,20 +9,23 @@ class Game:
         return rotated_surface, rotated_rect
 
     def move_car(self, surface_rect, angle, radius, direction):
+        x = radius * math.sin(math.pi * 2 * angle / 360)
+        y = radius * math.cos(math.pi * 2 * angle / 360)
+        digits = 0
         if direction == 'forward':
-            surface_rect.centerx -= radius * math.sin(math.pi * 2 * angle / 360)
-            surface_rect.centery -= radius * math.cos(math.pi * 2 * angle / 360)
+            surface_rect.centerx -= round(x, digits)
+            surface_rect.centery -= round(y, digits)
         elif direction == 'backward':
-            surface_rect.centerx += radius * math.sin(math.pi * 2 * angle / 360)
-            surface_rect.centery += radius * math.cos(math.pi * 2 * angle / 360)
+            surface_rect.centerx += round(x, digits)
+            surface_rect.centery += round(y, digits)
         return surface_rect
 
     def start_game(self):
         pygame.init()
         clock = pygame.time.Clock()
         screen = pygame.display.set_mode([900, 600])
-        track = pygame.image.load('track.png')
-        car = pygame.image.load('car.png')
+        track = pygame.image.load('track.png').convert()
+        car = pygame.image.load('car.png').convert()
         car_rotated = car
         car_rect = car.get_rect(center=(835, 535))
         angle = 0
@@ -56,14 +59,20 @@ class Game:
                         rotate_right = False
                     if event.key == pygame.K_LEFT:
                         rotate_left = False
+
             if rotate_right:
                 angle -= 10
+                if angle == -360:
+                    angle = 0
                 car_rotated, car_rect = self.rotate_car(car, car_rect, angle)
             if rotate_left:
                 angle += 10
+                if angle == 360:
+                    angle = 0
                 car_rotated, car_rect = self.rotate_car(car, car_rect, angle)
             if move_forward:
                 car_rect = self.move_car(car_rect, angle, step, 'forward')
+                print(car_rect.centerx)
             if move_backward:
                 car_rect = self.move_car(car_rect, angle, step, 'backward')
 
