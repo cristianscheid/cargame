4b9071450605dde5083d89aac191cf00a2a1b75e
cristian <cristian@debian>
2021-08-09 10:07:22 -0300

    implemented acceleration

--- game.py
+++ game.py
@@ -29,9 +29,9 @@ class Game:
         car_rotated = car
         car_rect = car.get_rect(center=(835, 535))
         angle = 0
-        step = 5
-
-        move_forward = move_backward = rotate_right = rotate_left = False
+        step = 0
+        direction = 'forward'
+        move_forward = move_backward = rotate_right = rotate_left = hand_brake = False
 
         while True:
             for event in pygame.event.get():
@@ -50,6 +50,8 @@ class Game:
                         rotate_right = True
                     if event.key == pygame.K_LEFT:
                         rotate_left = True
+                    if event.key == pygame.K_SPACE:
+                        hand_brake = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_UP:
                         move_forward = False
@@ -59,23 +61,35 @@ class Game:
                         rotate_right = False
                     if event.key == pygame.K_LEFT:
                         rotate_left = False
+                    if event.key == pygame.K_SPACE:
+                        hand_brake = False
 
-            if rotate_right:
-                angle -= 10
-                if angle == -360:
-                    angle = 0
-                car_rotated, car_rect = self.rotate_car(car, car_rect, angle)
-            if rotate_left:
+            if rotate_right and step != 0:
+                angle -= 10 
+            if rotate_left and step != 0:
                 angle += 10
-                if angle == 360:
-                    angle = 0
-                car_rotated, car_rect = self.rotate_car(car, car_rect, angle)
-            if move_forward:
-                car_rect = self.move_car(car_rect, angle, step, 'forward')
-                print(car_rect.centerx)
-            if move_backward:
-                car_rect = self.move_car(car_rect, angle, step, 'backward')
 
+            car_rotated, car_rect = self.rotate_car(car, car_rect, angle)
+
+            if move_forward:
+                step += 0.1
+            if move_backward:
+                step -= 0.1
+            if hand_brake:
+                if step > 0:
+                    step -= 0.2
+                if step < 0:
+                    step += 0.2
+            if move_forward == move_backward == hand_brake == False:
+                if step > 0:
+                    step -= 0.1
+                elif step < 0:
+                    step += 0.1
+
+            step = round(step, 2)
+            print(step)
+            
+            car_rect = self.move_car(car_rect, angle, step, direction)
             screen.blit(track, (0, 0))
             screen.blit(car_rotated, car_rect)
             pygame.display.flip()
